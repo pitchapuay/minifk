79ab96cdf3511dc267061b0b8a8acd5760295dd6
pitchapuay <wooncheek.dh@hotmail.com>
2020-05-30 20:05:46 +0700

    edit port

--- team18/client/src/http-common.js
+++ team18/client/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const SERVER_URL = process.env.SERVER_URL || 'http://172.17.0.200:9000';
 
 export default axios.create({
-    baseURL: "172.17.0.201:9000",
+    baseURL: SERVER_URL,
     headers: {
         'Access-Control-Allow-Origin': '*',
         "Content-type": "application/json",
